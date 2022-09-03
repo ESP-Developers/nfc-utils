f0c917f998ba3a3f488a85fcee7f735c6794346c
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-09-03 15:54:13 +0800

    define core methods

---
+++ main.js
@@ -0,0 +1,18 @@
+// check for NFC capabilities on web platform
+const checkNFCavailable = () => {
+    return true
+}
+
+// request permissions to enable NFC
+const requestPermissions = () => {
+    return true
+}
+
+// scan for NFC tags
+const scanNFC = () => {
+    return true
+}
+
+const writeNFC = () => {
+    return true
+}
